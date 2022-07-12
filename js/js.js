af7a41f4bc0a6dd1e39a50879b0306c090dee4a4
Matthew Meadows <Matthewmeadows4@gmail.com>
2022-07-12 15:44:11 -0400

    added fields to contact form

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
+    let x = document.forms["myForm"]["email"].value;
     if (x == "") {
-      alert("Hey! The form must be filled out!");
+      alert("Hey! Email field must be filled out");
       return false;
     }
   }
