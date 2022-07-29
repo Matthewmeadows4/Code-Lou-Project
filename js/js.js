f4079c22e1dcb18995503b25cb2723e046ae3cf2
Matthew Meadows <Matthewmeadows4@gmail.com>
2022-07-29 12:06:35 -0400

    Fixed contact

--- js/js.js
+++ js/js.js
@@ -1,7 +1,12 @@
+
+
 function validateForm() {
     let x = document.forms["myForm"]["email"].value;
     if (x == "") {
-      alert("Hey! Email field must be filled out");
-      return false;
+     alert("Hey! Email field must be filled out");
+      return false;}
     }
-  }
+    function submitForm() {
+      let name = document.getElementById("fname").value;
+      alert(`Thanks, ${name}! I will be getting back to you!`);
+    }
