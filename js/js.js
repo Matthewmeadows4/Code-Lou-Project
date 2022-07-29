d65522de228f0ce2f5f22f5e74c081e3ff41df4d
Matthew Meadows <Matthewmeadows4@gmail.com>
2022-07-29 12:57:31 -0400

    fixed form validation

--- js/js.js
+++ js/js.js
@@ -1,12 +1,8 @@
+function submitForm() {
+    let name = document.getElementById("fname").value;
+    if (name == "")
+    alert(`Hey, i need your name!`)
+    else 
+    alert(`Thanks, ${name}! I will be getting back to you!` );
+  }
 
-
-function validateForm() {
-    let x = document.forms["myForm"]["email"].value;
-    if (x == "") {
-     alert("Hey! Email field must be filled out");
-      return false;}
-    }
-    function submitForm() {
-      let name = document.getElementById("fname").value;
-      alert(`Thanks, ${name}! I will be getting back to you!`);
-    }
