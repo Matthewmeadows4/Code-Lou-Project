0417686962ecee45f7bf4c04eaa4290c706c2214
Matthew Meadows <Matthewmeadows4@gmail.com>
2022-07-29 13:02:29 -0400

    Contact rework

--- js/js.js
+++ js/js.js
@@ -1,8 +1,8 @@
 function submitForm() {
     let name = document.getElementById("fname").value;
     if (name == "")
-    alert(`Hey, i need your name!`)
+    alert(`Hey, I need your name!`)
     else 
-    alert(`Thanks, ${name}! I will be getting back to you!` );
+    alert(`Thanks, ${name}! I will be getting back to you!`);
   }
 
